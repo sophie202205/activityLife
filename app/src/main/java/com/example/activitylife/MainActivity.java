55fde99102a8de71c3e68fcf796e515f52eba024
sophie202205 <sophie6995137@gmail.com>
2022-05-14 20:29:03 +0800

    init

---
+++ app/src/main/java/com/example/activitylife/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.activitylife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void test1(View view) {
+        finish();
+    }
+}
