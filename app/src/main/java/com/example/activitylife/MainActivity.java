6465965facbc6e8d4cb3eb828dd3c3509955e4dc
sophie202205 <sophie6995137@gmail.com>
2022-05-14 20:36:02 +0800

    finish() change

--- app/src/main/java/com/example/activitylife/MainActivity.java
+++ app/src/main/java/com/example/activitylife/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.activitylife;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +13,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v("brad", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v("brad", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v("brad", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v("brad", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v("brad", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v("brad", "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.v("brad", "onRestart");
     }
 
     public void test1(View view) {
         finish();
+    }
+
+    @Override
+    public void finish() {
+        //super.finish();
+        Toast.makeText(this, "one more time", Toast.LENGTH_SHORT).show();
     }
 }
